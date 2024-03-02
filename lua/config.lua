251913344ff7b85da73d30fc72c6df9c99954070
0x736561 <dfern3321@gmail.com>
2024-03-01 22:20:26 -0300

    Treesitter configurado + correções

--- lua/config.lua
+++ lua/config.lua
@@ -1,8 +1,13 @@
 require('lualine').setup()
 require("bufferline").setup()
 require("gitsigns").setup()
-require("dashboard").setup {
-    shortcut_type = "number"
+require("dashboard").setup { shortcut_type = "number" }
+require("nvim-treesitter.configs").setup {
+    ensure_installed = { 
+        "c", "lua", "vim", "rust", "bash", "markdown", "org", "xml",
+        "html", "css", "javascript", "ini", "toml", "yaml"
+    },
+    highlight = { enable = true },
 }
 require'netrw'.setup {
   icons = {
@@ -17,77 +22,59 @@ require("themery").setup({
     {
         name = "Gruvbox Dark",
         colorscheme = "gruvbox",
-        before = [[
-            vim.opt.background = "dark"
-        ]],
+        before = [[ vim.opt.background = "dark" ]],
     },
     
     {
         name = "Gruvbox Light",
         colorscheme = "gruvbox",
-        before = [[
-            vim.opt.background = "light"
-        ]],
+        before = [[ vim.opt.background = "light" ]],
     },
     
     {
         name = "Everforest Dark",
         colorscheme = "everforest",
-        before = [[
-            vim.opt.background = "dark"
-        ]],
+        before = [[ vim.opt.background = "dark" ]],
     },
     
     {
         name = "Everforest Light",
         colorscheme = "everforest",
-        before = [[
-            vim.opt.background = "light"
-        ]],
+        before = [[ vim.opt.background = "light" ]],
     },
     
     {
         name = "Onedark",
         colorscheme = "onedark",
-        before = [[
-            vim.opt.background = "dark"
-        ]],
+        before = [[ vim.opt.background = "dark" ]],
     },
 
     {
         name = "Catppuccin Mocha",
         colorscheme = "catppuccin-mocha",
-        before = [[
-            vim.opt.background = "dark"
-        ]],
+        before = [[ vim.opt.background = "dark" ]],
     },
 
     {
         name = "Catppuccin Macchiato",
         colorscheme = "catppuccin-macchiato",
-        before = [[
-            vim.opt.background = "dark"
-        ]],
+        before = [[ vim.opt.background = "dark" ]],
     },
 
     {
         name = "Catppuccin Frappe",
         colorscheme = "catppuccin-frappe",
-        before = [[
-            vim.opt.background = "dark"
-        ]],
+        before = [[ vim.opt.background = "dark" ]],
     },
 
     {
         name = "Catppuccin Latte",
         colorscheme = "catppuccin-latte",
-        before = [[
-            vim.opt.background = "light"
-        ]],
+        before = [[ vim.opt.background = "light" ]],
     },
 
     },
-    themeConfigFile = "~/.config/nvim/lua/config/theme.lua",
+    themeConfigFile = "~/.config/nvim/lua/theme.lua",
     livePreview = true,
 })
 
--- lua/theme.lua
+++ lua/theme.lua
@@ -2,7 +2,6 @@
 -- This block will be replaced by Themery.
 vim.opt.background = "dark"
 
-
-vim.cmd("colorscheme gruvbox")
-vim.g.theme_id = 1
+vim.cmd("colorscheme everforest")
+vim.g.theme_id = 3
 -- end themery block
