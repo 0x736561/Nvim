0aefe7ced1ab047f6f8c0ed4dff525c6fb76b1e4
0x736561 <dfern3321@gmail.com>
2024-03-17 00:49:51 -0300

    Pequenas atualizações

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,6 @@
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.gopls.setup{}
 require'lspconfig'.lua_ls.setup{}
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.html.setup{}
+require'lspconfig'.bashls.setup{}
