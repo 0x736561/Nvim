1ca75f678a6f6628bba2c137570963cd7209d51f
0x736561 <dfern3321@gmail.com>
2024-03-27 20:58:09 -0300

    Novo tema

--- lua/theme.lua
+++ lua/theme.lua
@@ -2,6 +2,6 @@
 -- This block will be replaced by Themery.
 vim.opt.background = "dark"
 
-vim.cmd("colorscheme gruvbox")
-vim.g.theme_id = 1
+vim.cmd("colorscheme terafox")
+vim.g.theme_id = 16
 -- end themery block
